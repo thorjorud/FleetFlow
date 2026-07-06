234cbfc44d6e389bf2308db48291fbc5cbc1b80d
Thor Jorud <thorjorud06@gmail.com>
2026-07-06 11:16:58 -0500

    feat: add inventory api

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello FleetFlow');
 });
@@ -14,6 +16,18 @@ app.get('/inventory', async (req, res) => {
     } catch (err) {
         console.error('Error executing query:', err.stack);
         res.status(500).json({ error: 'Internal Server Error'});
+    }
+});
+
+app.post('/inventory', async (req, res) => {
+    try {
+        const {name, quantity, status} = req.body;
+        const result = await pool.query(
+            'INSERT INTO inventory (name, quantity, status) VALUES ($1, $2, $3) RETURNING *', [name, quantity, status]);
+            res.status(201).json(result.rows[0]);
+    } catch (err) {
+        console.error('Error executing query:', err.stack);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
