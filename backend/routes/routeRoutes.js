3e93b0f9843efe43eb5f807c4689165979bfeaf0
Thor Jorud <thorjorud06@gmail.com>
2026-07-12 13:53:16 -0500

    refactor: move inventory queries to mvc models

--- backend/routes/routeRoutes.js
+++ backend/routes/routeRoutes.js
@@ -2,12 +2,15 @@ import express from "express";
 import { getRoutes, getRouteById, createRoute, deleteRoute, updateRouteStatus } from "../controllers/routesController.js";
 
 const router = express.Router();
- 
-router.get('/', getRoutes);
-router.get('/:id', getRouteById);
-router.put('/:id', updateRouteStatus);
-router.delete('/:id', deleteRoute);
-router.post('/', createRoute);
+
+router.route('/')
+    .get(getRoutes)
+    .post(createRoute);
+
+router.route('/:id')
+    .get(getRouteById)
+    .put(updateRouteStatus)
+    .delete(deleteRoute);
 
 
 
